39ef2268e6b0808bbc896bef7bc0f18e18f5951f
Naved Hashmi <60292877+navedhashmi@users.noreply.github.com>
2025-08-17 19:12:41 +0530

    Add app/page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import LeftColumn from "@/app/components/left-column";
+import RightColumn from "@/app/components/right-column";
+
+export default function Home() {
+  return (
+    <>
+      <LeftColumn />
+      <RightColumn />
+    </>
+  );
+}
